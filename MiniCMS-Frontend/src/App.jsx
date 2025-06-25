d96ca744bdca9e5ecb6897cb29786c0557a2673a
shravanis30 <salunkeshravani561@gmail.com>
2025-06-25 07:00:51 +0530

    add routes

--- MiniCMS-Frontend/src/App.jsx
+++ MiniCMS-Frontend/src/App.jsx
@@ -1,24 +1,25 @@
 import React, { useEffect } from "react";
 import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "./firebase";import './App.css'
+import { auth } from "./firebase";
+import './App.css'
 import './index.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
-import EditPost from "./pages/EditPost";
-import AddPost from "./pages/AddPost";
-import { signInWithGoogle } from "./firebase.js"; // adjust path accordingly
+import { signInWithGoogle } from "./firebase.js"; 
 import { useNavigate } from "react-router-dom";
 import CreatePost from "./pages/CreatePost.jsx";
+import Sidebar from "./components/Sidebar.jsx";
+import ViewPosts from "./pages/ViewPosts.jsx";
 
 function App() {
 
   const handleLogin = async () => {
     try {
       const user = await signInWithGoogle();
-      navigate("/home"); // redirect after successful login
+      navigate("/home"); 
     } catch (err) {
       alert("Login failed: " + err.message);
     }
@@ -30,10 +31,10 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/home" element={<Home />} />      
-        <Route path="/editPost" element={<EditPost />} />      
-        <Route path="/addPost" element={<AddPost />} />      
+        <Route path="/home" element={<Home />} />        
         <Route path="/create-post" element={<CreatePost />} />      
+        <Route path="/sidebar" element={<Sidebar />} />      
+        <Route path="/view-posts" element={<ViewPosts />} />      
       </Routes>
     </Router>
   )
